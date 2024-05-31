b14559e984a7735494889f41e984e51bb0cb751f
denglei <denglei1024@hotmai.com>
2024-05-31 17:55:08 +0800

    创建项目

---
+++ backend/src/main/java/com/github/denglei1024/soloblogger/SoloBloggerApplication.java
@@ -0,0 +1,13 @@
+package com.github.denglei1024.soloblogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloBloggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloBloggerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/github/denglei1024/soloblogger/SoloBloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.denglei1024.soloblogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloBloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
