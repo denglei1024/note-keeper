bf37abe3ff844f105d669c696a8107bc167774f6
Ray <denglei1024@hotmail.com>
2024-06-01 21:27:21 +0800

    feat: 添加注册博主的接口

--- backend/src/main/java/com/github/denglei1024/soloblogger/controller/AdminController.java
+++ backend/src/main/java/com/github/denglei1024/soloblogger/controller/AdminController.java
@@ -1,7 +1,17 @@
 package com.github.denglei1024.soloblogger.controller;
 
+import com.github.denglei1024.soloblogger.dto.AdminSetupRequest;
+import com.github.denglei1024.soloblogger.model.User;
+import com.github.denglei1024.soloblogger.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * AdminController
@@ -12,4 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+    @Resource
+    private UserRepository userRepository;
+
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+    public ResponseEntity<String> setupAdmin(@RequestBody AdminSetupRequest request){
+        //检查是否存在博主账户
+        User admin = userRepository.findByUsername("admin");
+        if(!Objects.isNull(admin)){
+            return ResponseEntity.badRequest().body("博主账户已存在");
+        }
+
+        //创建博主账户
+        User newAdmin = new User();
+        newAdmin.setUsername("admin");
+        newAdmin.setPassword(passwordEncoder.encode(request.getPassword()));
+        newAdmin.setAdmin(true);
+        userRepository.save(newAdmin);
+
+        return ResponseEntity.ok("创建成功");
+    }
 }
